cdfa346c1cc05be7f67c5b37d7889126190ebf1d
Maxtremee <maxtremepl@gmail.com>
2021-04-29 19:18:08 +0200

    Initial commit

---
+++ src/main/java/com/pwr/faktury/FakturyApplication.java
@@ -0,0 +1,13 @@
+package com.pwr.faktury;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakturyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakturyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pwr/faktury/FakturyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pwr.faktury;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakturyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
