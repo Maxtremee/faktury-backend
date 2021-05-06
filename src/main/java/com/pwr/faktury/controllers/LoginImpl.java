d5d61a9c40366fc1e83f1cf430850f782109ec0d
Maxtremee <maxtremepl@gmail.com>
2021-05-06 14:40:50 +0200

    Backend: add basic functionality: login, signup (#2)
    * API definition transferred by SwaggerHub
    
    * API definition transferred by SwaggerHub
    
    * Begin signup implementation
    
    * Push from SwaggerHub.
    
    * API definition transferred by SwaggerHub
    
    * API definition transferred by SwaggerHub
    
    * API definition transferred by SwaggerHub
    
    * API definition transferred by SwaggerHub
    
    * API definition transferred by SwaggerHub
    
    * API definition transferred by SwaggerHub
    
    * Add basic functionality: login, signup
    
    * API definition transferred by SwaggerHub
    
    * Add roles management

---
+++ src/main/java/com/pwr/faktury/controllers/LoginImpl.java
@@ -0,0 +1,61 @@
+package com.pwr.faktury.controllers;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import com.pwr.faktury.api.LoginApiDelegate;
+import com.pwr.faktury.model.Login;
+import com.pwr.faktury.model.User;
+import com.pwr.faktury.model.UserDetails;
+import com.pwr.faktury.repositories.RoleRepository;
+import com.pwr.faktury.repositories.UserRepository;
+import com.pwr.faktury.security.jwt.JwtUtils;
+import com.pwr.faktury.security.services.UserDetailsImpl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoginImpl implements LoginApiDelegate {
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    @Autowired
+    PasswordEncoder encoder;
+
+    @Autowired
+    JwtUtils jwtUtils;
+
+
+    @Override
+    public ResponseEntity<UserDetails> login(Login login) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(login.getLogin(), login.getPassword()));
+        
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtUtils.generateJwtToken(authentication);
+
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        List<String> roles = userDetails.getAuthorities().stream().map(item -> item.getAuthority())
+                .collect(Collectors.toList());
+
+        Optional<User> user = userRepository.findByLogin(userDetails.getLogin());
+        UserDetails uDetails = new UserDetails().id(userDetails.getId()).login(userDetails.getLogin()).accessToken(jwt).roles(roles).tokenType("Bearer").company(user.get().getPersonal_data());
+        return new ResponseEntity<>(uDetails, HttpStatus.OK);
+    }
+    
+}
---
+++ src/main/java/com/pwr/faktury/controllers/SignupImpl.java
@@ -0,0 +1,86 @@
+package com.pwr.faktury.controllers;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import com.pwr.faktury.api.SignupApiDelegate;
+import com.pwr.faktury.model.ERole;
+import com.pwr.faktury.model.Role;
+import com.pwr.faktury.model.Roles;
+import com.pwr.faktury.model.Signup;
+import com.pwr.faktury.model.User;
+import com.pwr.faktury.repositories.RoleRepository;
+import com.pwr.faktury.repositories.UserRepository;
+import com.pwr.faktury.security.jwt.JwtUtils;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SignupImpl implements SignupApiDelegate {
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    @Autowired
+    PasswordEncoder encoder;
+
+    @Autowired
+    JwtUtils jwtUtils;
+
+    @Override
+    public ResponseEntity<Void> signup(Signup signup) {
+        if (userRepository.existsByLogin(signup.getLogin())) {
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+        
+        User user = new User();
+        user.setLogin(signup.getLogin());
+        user.setPassword(encoder.encode(signup.getPassword()));
+        user.setPersonal_data(signup.getCompany());
+
+        List<Roles> strRoles = signup.getRoles();
+        Set<Role> roles = new HashSet<>();
+        if (strRoles == null) {
+            Role userRole = roleRepository.findByName(ERole.USER)
+                    .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+            roles.add(userRole);
+        } else {
+            strRoles.forEach(role -> {
+                switch (role) {
+                    case ADMIN:
+                        Role adminRole = roleRepository.findByName(ERole.ADMIN)
+                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        roles.add(adminRole);
+
+                        break;
+                    case MODERATOR:
+                        Role modRole = roleRepository.findByName(ERole.MODERATOR)
+                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        roles.add(modRole);
+
+                        break;
+                    default:
+                        Role userRole = roleRepository.findByName(ERole.USER)
+                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        roles.add(userRole);
+                }
+            });
+        }
+        user.setRoles(roles);
+        
+        userRepository.save(user);
+
+        return new ResponseEntity<Void>(HttpStatus.OK);
+    }
+}
