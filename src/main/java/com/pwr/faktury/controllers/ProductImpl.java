33d800b37c4f56fdfcb91abc12fa155c81252c1b
Maxtremee <maxtremepl@gmail.com>
2021-05-09 22:48:36 +0200

    Finish implementing products endpoints

--- src/main/java/com/pwr/faktury/controllers/ProductImpl.java
+++ src/main/java/com/pwr/faktury/controllers/ProductImpl.java
@@ -1,5 +1,9 @@
 package com.pwr.faktury.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import com.pwr.faktury.api.ProductApiDelegate;
 import com.pwr.faktury.model.Product;
 import com.pwr.faktury.models.User;
@@ -28,6 +32,51 @@ public class ProductImpl implements ProductApiDelegate {
     AuthenticationManager authenticationManager;
 
     @Override
+    public ResponseEntity<Void> deleteProduct(String id) {
+        User user = userService.getUser();
+        if (user != null) {
+            for (Product temp : user.getProducts()) {
+                if (temp.getId().equals(id)) {
+                    productRepository.deleteById(id);
+                    user.getProducts().remove(temp);
+                    userRepository.save(user);
+                    return new ResponseEntity<Void>(HttpStatus.OK);
+                }
+            }
+            return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
+        } else {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+    }
+
+    @Override
+    public ResponseEntity<List<Product>> getProducts(String searchstr) {
+        User user = userService.getUser();
+        if (user != null) {
+            //return all if searchstr empty
+            if (searchstr == "" | searchstr.isEmpty()) {
+                return new ResponseEntity<>(new ArrayList<Product>(user.getProducts()), HttpStatus.OK);
+            }
+
+            //filter products with searchstr
+            List<Product> foundProducts = new ArrayList<>();
+            for (Product temp : user.getProducts()) {
+                if (temp.getName().contains(searchstr)) {
+                    foundProducts.add(temp);
+                }
+            }
+            //if none found return 404
+            if (foundProducts.size() > 0) {
+                return new ResponseEntity<>(foundProducts, HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } else {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+    }
+
+    @Override
     public ResponseEntity<Void> newProduct(Product product) {
         User user = userService.getUser();
         if (user != null) {
@@ -42,25 +91,26 @@ public class ProductImpl implements ProductApiDelegate {
             user.getProducts().add(product);
             userRepository.save(user);
             return new ResponseEntity<Void>(HttpStatus.CREATED);
-            
+
+        } else {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }
-        return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
     }
 
     @Override
-    public ResponseEntity<Void> deleteProduct(String id) {
+    public ResponseEntity<Void> updateProduct(String id, Product product) {
         User user = userService.getUser();
         if (user != null) {
-            for (Product temp : user.getProducts()) {
-                if (temp.getId().equals(id)) {
-                    productRepository.deleteById(id);
-                    user.getProducts().remove(temp);
-                    userRepository.save(user);
-                    return new ResponseEntity<Void>(HttpStatus.OK);
-                }
+            Optional<Product> temp = productRepository.findById(id);
+            if (temp.isPresent()) {
+                product.setId(id);
+                productRepository.save(product);
+                return new ResponseEntity<Void>(HttpStatus.OK);
+            } else {
+                return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
             }
-            return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
+        } else {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }
-        return new ResponseEntity<>(HttpStatus.UNAUTHORIZED); 
     }
 }
