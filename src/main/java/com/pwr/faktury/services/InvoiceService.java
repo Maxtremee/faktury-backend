103835a610a8d450d3fe1ff9474c774e3f4708fb
Maxtremee <maxtremepl@gmail.com>
2021-06-13 10:07:20 +0200

    Adapt to new Address model

--- src/main/java/com/pwr/faktury/services/InvoiceService.java
+++ src/main/java/com/pwr/faktury/services/InvoiceService.java
@@ -1,6 +1,8 @@
 package com.pwr.faktury.services;
 
 import java.io.ByteArrayInputStream;
+
+import com.pwr.faktury.model.Address;
 import com.pwr.faktury.model.Contractor;
 import com.pwr.faktury.model.Invoice;
 import com.pwr.faktury.model.InvoiceItem;
@@ -11,24 +13,38 @@ import org.springframework.stereotype.Service;
 @Service
 public class InvoiceService {
         public ByteArrayInputStream generatePdfFromId(Contractor seller, Invoice invoice) {
+                Address buyerAddress = invoice.getContractor().getAddress();
+                Address sellerAddress = seller.getAddress();
                 PdfInvoice pdfInvoice = new PdfInvoice();
 
                 pdfInvoice.setTitle(str(invoice.getTitle()));
-                pdfInvoice.setBuyerData(str(invoice.getContractor().getName()),
-                                str(invoice.getContractor().getFirstAddressLine()),
-                                str(invoice.getContractor().getSecondAddressLine()),
+                pdfInvoice.setBuyerData(
+                                str(invoice.getContractor().getName()),
+                                str(buyerAddress.getStreet()) + "," + str(buyerAddress.getPostalCode()) + " " + str(buyerAddress.getCity()),
+                                "tel. " + str(invoice.getContractor().getPhoneNumber()) + ", email: " + str(invoice.getContractor().getEmail()),
                                 str(invoice.getContractor().getNip()));
-                pdfInvoice.setSellerData(str(seller.getName()), str(seller.getFirstAddressLine()),
-                                str(seller.getSecondAddressLine()), str(seller.getNip()));
-                pdfInvoice.setInfoData(str(invoice.getIssuePlace()), str(invoice.getIssueDate()),
+                pdfInvoice.setSellerData(
+                                str(seller.getName()),
+                                str(sellerAddress.getStreet()) + "," + str(sellerAddress.getPostalCode()) + " " + str(sellerAddress.getCity()),
+                                "tel. " + str(seller.getPhoneNumber()) + " , email: " + str(seller.getEmail()),
+                                str(seller.getNip()));
+                pdfInvoice.setInfoData(
+                                str(invoice.getIssuePlace()), 
+                                str(invoice.getIssueDate()),
                                 str(invoice.getSellDate()));
-                pdfInvoice.setPaymentDetails(str(invoice.getPaymentType()), str(invoice.getPaymentDate()),
-                                str(seller.getBankName()), str(seller.getBankAccountNumber()));
+                pdfInvoice.setPaymentDetails(
+                                str(invoice.getPaymentType()),
+                                str(invoice.getPaymentDate()),
+                                str(seller.getBankName()), 
+                                str(seller.getBankAccountNumber()));
                 for (InvoiceItem item : invoice.getProducts()) {
-                        pdfInvoice.addProduct(str(item.getName()), item.getQuantity().floatValue(), str(item.getUnit()),
-                                        item.getPrice().floatValue(), item.getTax());
+                        pdfInvoice.addProduct(
+                                str(item.getName()),
+                                item.getQuantity().floatValue(),
+                                str(item.getUnit()),
+                                item.getPrice().floatValue(),
+                                item.getTax());
                 }
-
                 return pdfInvoice.createInvoice();
         }
 
